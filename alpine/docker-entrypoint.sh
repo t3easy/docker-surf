a644609809ab7511b025e74e2a8cb6a435577ffe
Jan Kiesewetter <jan@t3easy.de>
2016-11-09 12:26:12 +0100

    [BUGFIX] Don’t invoke “sh” through surf

--- alpine/docker-entrypoint.sh
+++ alpine/docker-entrypoint.sh
@@ -8,7 +8,7 @@ fi
 
 # if our command is a valid surf subcommand, let's invoke it through surf instead
 # (this allows for "docker run t3easy/surf list", etc)
-if surf "$1" --help > /dev/null 2>&1; then
+if [ "$1" != "sh" ] && surf "$1" --help > /dev/null 2>&1 ]; then
 	set -- surf "$@"
 fi
 
